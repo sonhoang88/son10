a7d99af27ec4503437278b139448ecadbff87d84
phamtienanh01 <153928669+phamtienanh01@users.noreply.github.com>
2023-12-24 20:43:52 +0700

    Update MyComponent.jsx

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -34,7 +34,7 @@ const MyComponent = () => {
 
       // Gửi dữ liệu đến endpoint
       const response = await axios.post(
-        'https://ap-southeast-1.aws.data.mongodb-api.com/app/data-fmace/endpoint/post_1',
+        'https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-rxqgf/endpoint/postmanual',
         formData
       );
 
